bbb818d87cab74afe74b575db4ff8aa0f3f11f8f
Alex Rincon <alejandro.rincon9@gmail.com>
2016-08-20 20:59:14 -0500

    Add better handling of image urls so we always get a correct amount

--- lib/bing_interface.rb
+++ lib/bing_interface.rb
@@ -2,13 +2,15 @@ class BingInterface
 	def self.return_image_urls(keyword, size)
 		client = BingSearch::Client.new(account_key: ENV["BING_SEARCH_ACCOUNT_KEY"])
 
-		image_results = client.image(keyword, limit: size)
+		image_results = client.image(keyword)
 		image_urls = []
 
 		image_results.each do |image|
 			image_urls << image.media_url
 		end
 
-		return image_urls
+		sample = image_urls.sample(size)
+
+		return sample
 	end
 end
